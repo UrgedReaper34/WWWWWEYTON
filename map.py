aff52ca7770672881c10fa0e8f3b7a7c4a7d0666
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2024-10-15 10:08:11 +0000

    [!refactor] invalid positions causes Map to raise ValueError

--- map.py
+++ map.py
@@ -31,11 +31,15 @@ class Map:
         self.set_grid(boss_x, boss_y, sym.BOSS)
 
     def get_grid(self, x: int, y: int) -> str:
-        """Return the character at the given position."""
+        """Return the character at the given position.
+        (x, y) is assumed to be a valid position.
+        """
         return self.grid[y][x]
 
     def set_grid(self, x: int, y: int, char: str) -> None:
-        """Set the character at the given position."""
+        """Set the character at the given position.
+        (x, y) is assumed to be a valid position.
+        """
         self.grid[y][x] = char
 
     def is_valid_coord(self, x: int, y: int) -> bool:
@@ -44,13 +48,15 @@ class Map:
         """
         return 0 <= x < self.width and 0 <= y < self.height
 
+    def validate_coord(self, x: int, y: int) -> None:
+        if not self.is_valid_coord(x, y):
+            raise ValueError(f"Position ({x}, {y}) is out of bounds!")
+
     def get_player_position(self) -> Position:
         return self.player_position
 
     def set_player_position(self, new_x: int, new_y: int) -> None:
-        if not self.is_valid_coord(new_x, new_y):
-            print(f"Position ({new_x}, {new_y}) is out of bounds!")
-            return
+        self.validate_coord(new_x, new_y)
         old_x, old_y = self.player_position
         if self.grid[old_y][old_x] == sym.PLAYER:
             self.grid[old_y][old_x] = sym.EMPTY
@@ -59,16 +65,12 @@ class Map:
 
     def set_monsters_position(self, x: int, y: int) -> None:
         """Put a monster at the given position."""
-        if not self.is_valid_coord(x, y):
-            print(f"Position ({x}, {y}) is out of bounds!")
-            return
+        self.validate_coord(x, y)
         self.set_grid(x, y, sym.MONSTER)
 
     def set_item_position(self, x: int, y: int) -> None:
         """Put an item at the given position."""
-        if not self.is_valid_coord(x, y):
-            print(f"Position ({x}, {y}) is out of bounds!")
-            return
+        self.validate_coord(x, y)
         self.set_grid(x, y, sym.ITEM)
 
     def display_map(self):
